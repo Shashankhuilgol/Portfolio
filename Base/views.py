07d5cfa2c1297e11013f4817d1fb15dc6c1ba706
unknown <Shashank.huilgol22@gmail.com>
2024-12-24 20:08:14 +0530

    changed the name

--- Base/views.py
+++ Base/views.py
@@ -21,30 +21,30 @@ def contact(request):
           pass
         else:
            messages.error(request,'Length of name the should be greater than 1 and less than 30 words.')
-           return render(request,'home.html')
+           return render(request,'index.html')
     
         if len(email)>1 and len(email)<30:
            pass
         else:
            messages.error(request,'Invalid email try again.')
-           return render(request,'home.html')
+           return render(request,'index.html')
 
         if len(number)>1 and len (number)<14:
            pass
         else:
            messages.error(request,'Invalid number try again.')
-           return render(request,'home.html')
+           return render(request,'index.html')
     
         if not content or len(content)<5:
            messages.error(request,'Content should be atleast 5 characters long.')
-           return render(request,'home.html')
+           return render(request,'index.html')
     
         ins=models.Contact(name=name,email=email,number=number,content=content)
         ins.save()
         messages.success(request,'Thank you for contacting me || your message have been saved')
         print('Data has been saved to database')
-        return render(request,'home.html')
-    return render(request,'home.html')
+        return render(request,'index.html')
+    return render(request,'index.html')
     
 
 
