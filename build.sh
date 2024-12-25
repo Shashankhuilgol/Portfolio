32d89db42944038a1d8b48387676da6bcea8c435
unknown <Shashank.huilgol22@gmail.com>
2024-12-26 00:03:31 +0530

    .

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python ./www/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
+
+python manage.py makemigrations
 python manage.py migrate
