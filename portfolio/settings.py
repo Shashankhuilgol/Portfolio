1449eda61c99712bfebb8405af25eaa9c7e697e9
unknown <Shashank.huilgol22@gmail.com>
2024-12-25 12:25:46 +0530

    pushing to render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_+e1n8b9@*vxf653jm&(@rsh!h^lb^&70n5%ifgqrops=389o7'
+SECRET_KEY ='django-insecure-_+e1n8b9@*vxf653jm&(@rsh!h^lb^&70n5%ifgqrops=389o7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
