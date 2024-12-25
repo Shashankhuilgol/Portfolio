4bf672e4515c3a5b1f68e659ae4e030c048c5776
unknown <Shashank.huilgol22@gmail.com>
2024-12-26 01:01:45 +0530

    removed files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ='django-insecure-_+e1n8b9@*vxf653jm&(@rsh!h^lb^&70n5%ifgqrops=389o7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "https://portfolio-20n4.onrender.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
