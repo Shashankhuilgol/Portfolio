67621aeaa4ebbc31af30cf6f08d59e6487b71115
unknown <Shashank.huilgol22@gmail.com>
2024-12-27 00:17:07 +0530

    modified

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY ='django-insecure-_+e1n8b9@*vxf653jm&(@rsh!h^lb^&70n5%ifgqrops=389o7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
