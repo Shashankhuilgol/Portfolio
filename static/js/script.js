d184e898c7f57c930bbb321f5e062283dad7237b
unknown <Shashank.huilgol22@gmail.com>
2025-03-22 18:11:30 +0530

    modified

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("JavaScript Loaded!"); // Check in the browser console
+    document.getElementById("loading-screen").style.display = "none";
+});
+
