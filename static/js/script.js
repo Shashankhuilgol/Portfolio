efe68b741e590740a2fac4d7d6a17a1ab9206ce9
unknown <Shashank.huilgol22@gmail.com>
2025-03-24 18:14:25 +0530

    modified the portfolio project section

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("JavaScript Loaded!"); // Check in the browser console
+    console.log("JavaScript Loaded!");
     document.getElementById("loading-screen").style.display = "none";
-});
 
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3, // Show 3 cards at a time
+      spaceBetween: 20, // Spacing between slides
+      loop: false, // No infinite scrolling
+      autoplay: {
+          delay: 3000, // Auto-slide every 3s
+          disableOnInteraction: false,
+      },
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+      },
+      breakpoints: {
+          1024: {
+              slidesPerView: 3, 
+          },
+          768: {
+              slidesPerView: 2, 
+          },
+          480: {
+              slidesPerView: 1, 
+          }
+      }
+  });
+  
+      
+});
