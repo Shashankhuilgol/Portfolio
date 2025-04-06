c8130b80a782170b0cb81027d352a4b97263ef79
unknown <Shashank.huilgol22@gmail.com>
2025-04-07 01:02:40 +0530

    modified'

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ='django-insecure-_+e1n8b9@*vxf653jm&(@rsh!h^lb^&70n5%ifgqrops=389o7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
